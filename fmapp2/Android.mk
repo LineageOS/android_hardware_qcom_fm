4c872ab131c2d700760b03cf993406470b8f1600
Arne Coucheron <arco68@gmail.com>
2014-02-02 09:40:16 +0100

    Drop BOARD_HAVE_QCOM_FM flag
    * Only devices supporting this will build it anyway.
    
    Change-Id: Ic0569d7c8c37427150c2f1dc6e3d1040d619dfc1

--- fmapp2/Android.mk
+++ fmapp2/Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
-
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -33,5 +31,3 @@ LOCAL_JNI_SHARED_LIBRARIES := libqcomfm_jni
 LOCAL_JAVA_LIBRARIES := qcom.fmradio
 
 include $(BUILD_PACKAGE)
-
-endif
