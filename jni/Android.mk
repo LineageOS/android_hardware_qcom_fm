213d4c69ee7a9c1ea57791b15c61efd34dbda169
Arne Coucheron <arco68@gmail.com>
2014-02-02 09:40:16 +0100

    Drop BOARD_HAVE_QCOM_FM flag
    * Only devices supporting this will build it anyway.
    
    Change-Id: Ic0569d7c8c37427150c2f1dc6e3d1040d619dfc1

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,4 +1,3 @@
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
 ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -28,4 +27,3 @@ include $(BUILD_SHARED_LIBRARY)
 
 
 endif # is-vendor-board-platform
-endif # BOARD_HAVE_QCOM_FM
