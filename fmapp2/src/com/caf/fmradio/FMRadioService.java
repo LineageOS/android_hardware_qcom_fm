f986add38f33ad14d6706be0d3613d69a54c674a
Edward Wang <ewang@cyngn.com>
2015-04-09 16:06:02 -0700

    FM: retune last frequency when resume after call
    FM Activity may not available to send down frequency again
    after phone call. Then FM service need retune saved frequency.
    
    Change-Id: I26f0921bd8878331c3dedc6e8ff96873dd5a84a6
    (cherry picked from commit 4b86c869026d6ef7504d2cecca4c3db9fa9d8c4b)

--- fmapp2/src/com/caf/fmradio/FMRadioService.java
+++ fmapp2/src/com/caf/fmradio/FMRadioService.java
@@ -138,6 +138,7 @@ public class FMRadioService extends Service
    private int mServiceStartId = -1;
    private boolean mServiceInUse = false;
    private static boolean mMuted = false;
+   private static int mFreq = 0;
    private static boolean mResumeAfterCall = false;
    private static String mAudioDevice="headset";
    MediaRecorder mRecorder = null;
@@ -1539,6 +1540,8 @@ public class FMRadioService extends Service
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
+            } else if (mFreq > 0) {
+                tune(mFreq);
             }
         }
     }
@@ -2852,6 +2855,7 @@ public class FMRadioService extends Service
       {
          mReceiver.setStation(frequency);
          bCommandSent = true;
+         mFreq = frequency;
       }
       return bCommandSent;
    }
