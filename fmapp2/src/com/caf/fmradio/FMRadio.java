0455f6f64a61d065437a7d91bda1e7146e2078f9
Edward Wang <ewang@cyngn.com>
2015-04-13 14:17:00 -0700

    FM: reenable radio text visibility after station info updated
    After radio station info updated, radio text view is set to GONE.
    We need turn it back on if FM is still enabled.
    
    Change-Id: I7acea564a352693e19d9e6d934d641327b90be7e
    (cherry picked from commit 086a510710b073b4b5c5cd4a0f72341dc6690b84)

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -1686,7 +1686,6 @@ public class FMRadio extends Activity
                 bStatus = mService.fmOn();
                 if(bStatus) {
                    tuneRadio(FmSharedPreferences.getTunedFrequency());
-                   enableRadioOnOffUI();
                 }else {
                    Log.e(LOGTAG, "mService.fmOn failed");
                    mCommandFailed = CMD_FMON;
@@ -2631,6 +2630,7 @@ public class FMRadio extends Activity
              mTunedStation.setPI(0);
              mTunedStation.setPty(0);
              updateStationInfoToUI();
+             enableRadioOnOffUI();
          }catch (RemoteException e) {
             e.printStackTrace();
          }
@@ -2688,6 +2688,7 @@ public class FMRadio extends Activity
       mERadioTextScroller.stopScroll();
       mUpdatePickerValue = true;
       updateStationInfoToUI();
+      enableRadioOnOffUI();
    }
 
    Runnable mRadioEnabled = new Runnable() {
