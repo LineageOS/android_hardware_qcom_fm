2a18665ed424b0d2a0ce7641509c9761352aea29
Ed Falk <efalk@cyngn.com>
2015-10-07 11:57:52 -0700

    FM: Fix headphone icon
    Original headphones were mis-matched w.r.t. default/selected. This
    change normalizes the icon. Also fixes a couple compiler warnings
    in FMRadio.java
    
    Change-Id: Iee383bc23455562f21c3c431d5b291506e0e3e13

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -660,7 +660,6 @@ public class FMRadio extends Activity
       MenuItem item;
       boolean radioOn = isFmOn();
       boolean recording = isRecording();
-      boolean mSpeakerPhoneOn = isSpeakerEnabled();
       boolean sleepActive = isSleepTimerActive();
       boolean searchActive = isScanActive() || isSeekActive();
 
@@ -718,7 +717,6 @@ public class FMRadio extends Activity
       boolean radioOn = isFmOn();
       boolean recording = isRecording();
       boolean RtPlusSupported = isRtPlusSupported();
-      boolean mSpeakerPhoneOn = isSpeakerEnabled();
       boolean searchActive = isScanActive() || isSeekActive();
 
       item = menu.findItem(MENU_SCAN_START);
@@ -813,7 +811,7 @@ public class FMRadio extends Activity
    }
 
    private void enableSpeaker() {
-    //This method with toggle Speaker phone based on existing state .
+       //This method with toggle Speaker phone based on existing state .
        boolean bSpeakerPhoneOn = isSpeakerEnabled();
        if(mService != null) {
            try {
@@ -1554,7 +1552,6 @@ public class FMRadio extends Activity
         new View.OnClickListener() {
           @Override
           public void onClick(View v) {
-             // TODO Auto-generated method stub
              mSpeakerButton.setClickable(false);
              mSpeakerButton.setOnClickListener(null);
              mHandler.removeCallbacks(mEnableRadioTask);
