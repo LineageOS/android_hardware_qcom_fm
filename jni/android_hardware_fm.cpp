35dd3181bfe905fb2a060addfd686413c1fb27e2
Ricardo Cerqueira <ricardo@cyngn.com>
2016-01-26 13:43:39 +0000

    jni: Confine the firmware-load skipping to the actual loading
    Change If18650af934f840a54f53fa9474a3341df0e3e68 removed the
    property-setting steps from the initialization routine. Bring it
    back, as we may want to use those properties to trigger module
    state changes from init. The recommended flow for devices with
    the latest IRIS driver is the following:
    
    on property:hw.fm.init=0
        write /sys/module/radio_iris_transport/parameters/fmsmd_set 0
    
    on property:hw.fm.init=1
        write /sys/module/radio_iris_transport/parameters/fmsmd_set 1
    
    This change mirrors the change done for libfm_jni:
    Id1997a51d83b82425630f6975f4020d4a2a448c4
    
    Change-Id: Ifddf678398fec2e4bfa9688c044da7d75898e056

--- jni/android_hardware_fm.cpp
+++ jni/android_hardware_fm.cpp
@@ -670,8 +670,8 @@ static jint android_hardware_fmradio_FmReceiverJNI_acquireFdNative
        /*Set the mode for soc downloader*/
        property_set("hw.fm.mode", "normal");
        /* Need to clear the hw.fm.init firstly */
-#ifndef QCOM_NO_FM_FIRMWARE
        property_set("hw.fm.init", "0");
+#ifndef QCOM_NO_FM_FIRMWARE
        property_set("ctl.start", "fm_dl");
        sched_yield();
        for(i=0; i<45; i++) {
@@ -683,6 +683,11 @@ static jint android_hardware_fmradio_FmReceiverJNI_acquireFdNative
             usleep(WAIT_TIMEOUT);
          }
        }
+#else
+       property_set("hw.fm.init", "1");
+       usleep(WAIT_TIMEOUT);
+       init_success = 1;
+#endif
        ALOGE("init_success:%d after %f seconds \n", init_success, 0.2*i);
        if(!init_success) {
          property_set("ctl.stop", "fm_dl");
@@ -690,9 +695,6 @@ static jint android_hardware_fmradio_FmReceiverJNI_acquireFdNative
          close(fd);
          return FM_JNI_FAILURE;
        }
-#else
-       usleep(WAIT_TIMEOUT);
-#endif
     }
     return fd;
 }
