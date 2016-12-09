83355d2e3c872f430003a052b49da56e4473f61b
David Ng <dave@codeaurora.org>
2016-12-09 00:17:00 -0800

    Disable module for pure AOSP configuration
    Module requires framework changes not supported by
    pure AOSP configuration.  Also fix const char*/char*
    assignment in libqcomfm_jni.
    
    Change-Id: I71d125230cd772725ecc27ea346c911ea2505e7e

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-#ifneq ($(TARGET_USES_AOSP),true)
+ifneq ($(TARGET_USES_AOSP),true)
 
 #ifeq ($(BOARD_HAVE_QCOM_FM),true)
 #ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
@@ -37,6 +37,6 @@ include $(LOCAL_PATH)/fmhalService/Android.mk
 
 #endif # is-vendor-board-platform
 #endif # BOARD_HAVE_QCOM_FM
-#endif # Not (TARGET_USES_AOSP)
+endif # Not (TARGET_USES_AOSP)
 LOCAL_PATH := $(LOCAL_DIR_PATH)
 include $(LOCAL_PATH)/libfm_jni/Android.mk
