eeacdcc2d94f5d57344caada9fbb5d90983ecec6
Ethan Chen <intervigil@gmail.com>
2017-01-16 12:14:00 -0800

    Use BOARD_HAVE_QCOM_FM flag
    * Prevent parsing any subdirectory files unless BOARD_HAVE_QCOM_FM is
      enabled.
    
    Change-Id: I7f4995b882bac3a6380026eaab2dedd57b5bb5b8

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(BOARD_HAVE_QCOM_FM),true)
+
 LOCAL_PATH:= $(call my-dir)
 LOCAL_DIR_PATH:= $(call my-dir)
 
@@ -10,3 +12,5 @@ include $(LOCAL_PATH)/fm_hci/Android.mk
 
 LOCAL_PATH := $(LOCAL_DIR_PATH)
 include $(LOCAL_PATH)/helium/Android.mk
+
+endif # BOARD_HAVE_QCOM_FM
