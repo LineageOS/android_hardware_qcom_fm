c1c29ea48680c38f8eece48c30a02b704b010d26
Kamal Negi <kamaln@codeaurora.org>
2017-08-04 15:46:32 +0530

    fm: Initialize variables after fm_hal service connection
    Initialize variables after fm_hal_service connection. In subsequent FM on,
    if hal init happens before threads exits in previous FM off, there might be
    race condition , because of which lib_running variable sets to 0 even if
    init happens. This will lead to FM failure as all threads exits as
    soon as created.
    
    Change-Id: I2e61cc7ebb935c0b31c575aed60ead1424d23eb7

--- fm_hci/fm_hci.c
+++ fm_hci/fm_hci.c
@@ -704,11 +704,6 @@ int fm_hci_init(fm_hci_hal_t *hci_hal)
     }
     memset(hci, 0, sizeof(struct fm_hci_t));
 
-    lib_running = 1;
-    ready_events = 0;
-    hci->command_credits = 1;
-    hci->fd = -1;
-
     pthread_mutex_init(&hci->tx_q_lock, NULL);
     pthread_mutex_init(&hci->credit_lock, NULL);
     pthread_mutex_init(&hci->event_lock, NULL);
@@ -723,6 +718,11 @@ int fm_hci_init(fm_hci_hal_t *hci_hal)
         goto err_socket;
     }
     ALOGI("fm_hal_fd = %d", fm_hal_fd);
+
+    lib_running = 1;
+    ready_events = 0;
+    hci->command_credits = 1;
+    hci->fd = -1;
 
     ret = vendor_init(hci);
     if (ret)
